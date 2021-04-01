1033de94129b60fe78649a0e54361da4f286bf01
diazmc <mc.arthur_d@hotmail.com>
2021-04-01 17:25:22 -0500

    implemented size function

--- linked-list/index.js
+++ linked-list/index.js
@@ -11,11 +11,25 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+
+    while(node) {
+      node = node.next;
+      count++;
+    }
+
+    return count;
+  }
 }
 
 let node1 = new LinkedListNode(2);
 let node2 = new LinkedListNode(5);
+let node3 = new LinkedListNode(10);
 node1.next = node2;
+node2.next = node3;
 
 let list = new LinkedList(node1);
-console.log(list)
+console.log(list.size());
