ce361b313025f54f40c703ff1619865052891f1e
diazmc <mc.arthur_d@hotmail.com>
2021-04-10 23:03:14 -0500

    remove duplicated from a linked list

---
+++ delete-duplicates-linked-list/index.js
@@ -0,0 +1,18 @@
+//Given the head of a sorted linked list, 
+//delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+function deleteDuplicates(head) {
+  if(head == null) retur null;
+
+  let current = head;
+
+  while(current != null && current.next != null) {
+    if(current.next.val == current.val) {
+      current.next = current.next.next;
+    } else{
+      current = current.next;
+    }
+  }
+
+  return head;
+}
