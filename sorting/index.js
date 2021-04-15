f84c3899fc14258d2c00c93ad6dd41d88eee3c3b
diazmc <mc.arthur_d@hotmail.com>
2021-04-14 21:20:03 -0500

    implement selectionSort

--- sorting/index.js
+++ sorting/index.js
@@ -3,9 +3,32 @@
 function bubbleSort(arr) {
   for(let i = 0; i < arr.length; i++) {
     for(let j = 0; j < (arr.length - i - 1); j++){
-      let temp = arr[j+1];
-      arr[j+1] = arr[j];
-      arr[j] = temp;
+      if(arr[j] > arr[j+1]) {
+        let temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+//Implement selectionSort
+
+function selectionSort(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if(indexOfMin !== i) {
+      let temp = arr[indexOfMin];
+      let arr[indexOfMin] = arr[i];
+      arr[i] = temp;
     }
   }
 
